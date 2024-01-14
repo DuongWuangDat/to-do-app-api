326698564dc5e9b3688d322862afd2babaa98a68
DuongWuangDat <duongquangdat172004@gmail.com>
2024-01-14 20:26:38 +0700

    finish

--- models/Task.go
+++ models/Task.go
@@ -1,8 +1,80 @@
 package models
 
-var Task struct {
-	ID        string `bson:"_id,omitempty" json:"_id,omitempty"`
-	Title     string `bson:"title" json:"title"`
-	IsDone    bool   `bson:"isdone" json:"isdone"`
-	CreatedAt int    `bson:"createdat" json:"createdat"`
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/DuongWuangDat/to-do-app-api/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Task struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Title     string             `bson:"title" json:"title"`
+	IsDone    bool               `bson:"isdone" json:"isdone"`
+	CreatedAt int                `bson:"createdat" json:"createdat"`
+}
+
+func GetAll() ([]Task, error) {
+	var tasks []Task
+	cur, err := database.Collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		return tasks, err
+	}
+	err = cur.All(context.Background(), &tasks)
+	return tasks, err
+}
+
+func GetOne(taskId string) (Task, error) {
+	id, err := primitive.ObjectIDFromHex(taskId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filter := bson.M{
+		"_id": id,
+	}
+	var task Task
+	err = database.Collection.FindOne(context.Background(), filter).Decode(&task)
+	return task, err
+
+}
+
+func (d *Task) CreateTask() (string, error) {
+	rs, err := database.Collection.InsertOne(context.TODO(), d)
+	return rs.InsertedID.(primitive.ObjectID).String(), err
+}
+
+func DeleteTask(taskId string) error {
+	id, err := primitive.ObjectIDFromHex(taskId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filter := bson.M{
+		"_id": id,
+	}
+	_, err = database.Collection.DeleteOne(context.Background(), filter)
+	return err
+
+}
+
+func (d *Task) UpdateTask(taskID string) error {
+	id, err := primitive.ObjectIDFromHex(taskID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filter := bson.M{
+		"_id": id,
+	}
+	update := bson.M{
+		"$set": bson.M{
+			"title":     d.Title,
+			"isdone":    d.IsDone,
+			"createdat": time.Now().Unix(),
+		},
+	}
+	_, err = database.Collection.UpdateOne(context.Background(), filter, update)
+	return err
+
 }
--- routes/Route.go
+++ routes/Route.go
@@ -1,20 +1,101 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/DuongWuangDat/to-do-app-api/models"
+	"github.com/gin-gonic/gin"
+)
 
 func GetAllTask(c *gin.Context) {
+	tasks, err := models.GetAll()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"data": tasks,
+	})
 
 }
 
 func GetOneTask(c *gin.Context) {
-
+	id := c.Param("id")
+	task, err := models.GetOne(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"data": task,
+	})
 }
 func DeleteTask(c *gin.Context) {
-
+	id := c.Param("id")
+	err := models.DeleteTask(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Delete successful",
+	})
 }
 func AddNewTask(c *gin.Context) {
-
+	var body models.Task
+	err := c.ShouldBindJSON(&body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	id, err := body.CreateTask()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Add successful",
+		"id":      id,
+	})
 }
 func UpdateTask(c *gin.Context) {
-
+	id := c.Param("id")
+	var body models.Task
+	err := c.ShouldBindJSON(&body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	task, err := models.GetOne(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	if body.Title == "" {
+		body.Title = task.Title
+	}
+	err = body.UpdateTask(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Update successful",
+	})
 }
